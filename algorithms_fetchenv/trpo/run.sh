457668d6e36e7ba3a61817c0af2a20ac8938cccb
Tung Luu <tungluu2203@kaist.ac.kr>
2019-04-25 20:22:27 +0900

    Remove TRPO module in TD3 branch

--- algorithms_fetchenv/trpo/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#python trpo.py --env FetchReach-v1 --logdir ../../logs/trpo_dense --epochs 1000
-#python trpo.py --env FetchPickAndPlace-v1 --logdir ../../logs/trpo_dense_1 --epochs 1000 --steps 1000
-#python trpo.py --env FetchPickAndPlace-v1 --logdir ../../logs/trpo_dense_3_layer_1 --epochs 1000 --steps 4000 --hid 256 --l 3 --cpu 8
-#python trpo.py --env FetchSlide-v1 --logdir ../../logs/trpo_dense --epochs 1000
-#python trpo.py --env FetchPush-v1 --logdir ../../logs/trpo_dense --epochs 1000 --cpu 4
-
-python test_policy.py --episodes 10 --generate_demo --demo_path ../imitatation_learning/data/ --saved_model ../../logs.bak/trpo_dense_FetchReach-v1/trpo/trpo_s0/ --logdir ../../logs.bak/trpo_dense_test --use_tensorboard
-#python test_policy.py --episodes 1000 --env FetchPickAndPlace-v1 --logdir ../../logs/trpo_dense_random --use_tensorboard --random_policy
