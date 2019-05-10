dec3b7482f411a22cbce4e6af8a58fc15f375e4a
Tung Luu <tungluu2203@kaist.ac.kr>
2019-05-10 19:26:11 +0900

    Refactor name in experience replay buffer class

--- external_packages/baselines/baselines/her/replay_buffer.py
+++ external_packages/baselines/baselines/her/replay_buffer.py
@@ -17,12 +17,12 @@ class ReplayBuffer:
         """
         self.buffer_shapes = buffer_shapes
         self.size_in_transitions = size_in_transitions  # Measure in number of transitions
-        self.size = size_in_transitions // T    # Measure in number of episodes
+        self.size_in_episodes = size_in_transitions // T    # Measure in number of episodes
         self.T = T
         self.sample_transitions = sample_transitions
 
         # self.buffers is {key: array(size_in_episodes x T or T+1 x dim_key)}
-        self.buffers = {key: np.empty([self.size, *shape])
+        self.buffers = {key: np.empty([self.size_in_episodes, *shape])
                         for key, shape in buffer_shapes.items()}
 
         # memory management
@@ -35,7 +35,7 @@ class ReplayBuffer:
     @property
     def full(self):
         with self.lock:
-            return self.current_size == self.size
+            return self.current_size == self.size_in_episodes
 
     def sample(self, batch_size):
         """Returns a dict {key: array(batch_size x shapes[key])}
@@ -91,20 +91,20 @@ class ReplayBuffer:
 
     def _get_storage_idx(self, inc=None):
         inc = inc or 1   # size increment
-        assert inc <= self.size, "Batch committed to replay is too large!"
+        assert inc <= self.size_in_episodes, "Batch committed to replay is too large!"
         # go consecutively until you hit the end, and then go randomly.
-        if self.current_size+inc <= self.size:
+        if self.current_size+inc <= self.size_in_episodes:
             idx = np.arange(self.current_size, self.current_size+inc)
-        elif self.current_size < self.size:
-            overflow = inc - (self.size - self.current_size)
-            idx_a = np.arange(self.current_size, self.size)
+        elif self.current_size < self.size_in_episodes:
+            overflow = inc - (self.size_in_episodes - self.current_size)
+            idx_a = np.arange(self.current_size, self.size_in_episodes)
             idx_b = np.random.randint(0, self.current_size, overflow)
             idx = np.concatenate([idx_a, idx_b])
         else:
-            idx = np.random.randint(0, self.size, inc)
+            idx = np.random.randint(0, self.size_in_episodes, inc)
 
         # update replay size
-        self.current_size = min(self.size, self.current_size+inc)
+        self.current_size = min(self.size_in_episodes, self.current_size+inc)
 
         if inc == 1:
             idx = idx[0]
