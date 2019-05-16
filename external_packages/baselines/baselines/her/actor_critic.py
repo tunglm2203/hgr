2355d6d5f1e4e821e663e66016fb6dcca71d9115
Tung Luu <tungluu2203@kaist.ac.kr>
2019-05-16 14:00:34 +0900

    Minor modify in comment

--- external_packages/baselines/baselines/her/actor_critic.py
+++ external_packages/baselines/baselines/her/actor_critic.py
@@ -3,9 +3,8 @@ from baselines.her.util import store_args, nn
 
 
 class ActorCritic:
-    @store_args
-    def __init__(self, inputs_tf, dimo, dimg, dimu, max_u, o_stats, g_stats, hidden, layers,
-                 **kwargs):
+    # @store_args
+    def __init__(self, inputs_tf, dimo, dimg, dimu, max_u, o_stats, g_stats, hidden, layers):
         """The actor-critic network and related training code.
 
         Args:
@@ -21,6 +20,16 @@ class ActorCritic:
             hidden (int): number of hidden units that should be used in hidden layers
             layers (int): number of hidden layers
         """
+        self.inputs_tf = inputs_tf
+        self.dimo = dimo
+        self.dimg = dimg
+        self.dimu = dimu
+        self.max_u = max_u
+        self.o_stats = o_stats
+        self.g_stats = g_stats
+        self.hidden = hidden
+        self.layers = layers
+
         self.o_tf = inputs_tf['o']
         self.g_tf = inputs_tf['g']
         self.u_tf = inputs_tf['u']
@@ -36,9 +45,9 @@ class ActorCritic:
                 input_pi, [self.hidden] * self.layers + [self.dimu]))
         with tf.variable_scope('Q'):
             # for policy training
-            input_Q = tf.concat(axis=1, values=[o, g, self.pi_tf / self.max_u])
-            self.Q_pi_tf = nn(input_Q, [self.hidden] * self.layers + [1])
+            input_q = tf.concat(axis=1, values=[o, g, self.pi_tf / self.max_u])
+            self.Q_pi_tf = nn(input_q, [self.hidden] * self.layers + [1])
             # for critic training
-            input_Q = tf.concat(axis=1, values=[o, g, self.u_tf / self.max_u])
-            self._input_Q = input_Q  # exposed for tests
-            self.Q_tf = nn(input_Q, [self.hidden] * self.layers + [1], reuse=True)
+            input_q = tf.concat(axis=1, values=[o, g, self.u_tf / self.max_u])
+            self._input_Q = input_q  # exposed for tests
+            self.Q_tf = nn(input_q, [self.hidden] * self.layers + [1], reuse=True)
